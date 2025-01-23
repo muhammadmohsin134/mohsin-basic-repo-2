a788f0cedc991f0a48ac1ce595ab8a06d9ee4a7b
mohsin <mohsinedu541@gmail.com>
2025-01-23 23:48:19 +0500

    product

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	double n1,n2,p;
+	cout<<"enter two numbers";
+	cin>>n1>>n2;
+	p=n1*n2;
+	cout<<"product of numbers is:"<<p;
+	return 0;
+}
