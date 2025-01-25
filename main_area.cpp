584146d695ba32b6246893f272aa7422b3ebebf5
mohsin <mohsinedu541@gmail.com>
2025-01-26 00:00:54 +0500

    area

---
+++ main_area.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int l,w,a;
+	cout<<"enter length ";
+	cin>>l;
+	cout<<"enter width";
+	cin>>w;
+	a=l*w;
+	cout<<"area of rectangle is:"<<a;
+	return 0;
+}
