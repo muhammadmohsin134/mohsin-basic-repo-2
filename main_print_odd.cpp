4bb7522e2bc672633d61fe14b3bd1f0646b416e3
mohsin <mohsinedu541@gmail.com>
2025-01-26 00:02:59 +0500

    print odd

---
+++ main_print_odd.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int i =1;
+	while(i<=10){
+	cout<<i<<endl;
+	i=i+2;	
+	}
+
+	return 0;
+}
