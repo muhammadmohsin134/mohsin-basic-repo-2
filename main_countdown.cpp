c8a06af6c9a40b856b4c70272d7a6cc9d6c23253
mohsin <mohsinedu541@gmail.com>
2025-01-26 00:01:54 +0500

    countdown

---
+++ main_countdown.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	int cd=10;
+	while(cd>0){
+		cout<<cd<<endl;
+		cd--;
+	}
+	cout<<"happy new year";
+	
+	return 0;
+}
